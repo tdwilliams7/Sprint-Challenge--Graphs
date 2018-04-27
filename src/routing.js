b94ca67882f687fccd9a98c6b32b8a69a213ab6b
Troy Williams <troyDonwilliams@gmail.com>
2018-04-27 12:25:23 -0500

    done

--- src/routing.js
+++ src/routing.js
@@ -4,7 +4,7 @@
  * Edge class
  */
 class Edge {
-  constructor(destination, weight=1) {
+  constructor(destination, weight = 1) {
     this.destination = destination;
     this.weight = weight;
   }
@@ -14,9 +14,10 @@ class Edge {
  * Vertex class
  */
 class Vertex {
-  constructor(value='vertex') {
+  constructor(value = 'vertex') {
     this.value = value;
     this.edges = [];
+    this.parent = null;
   }
 }
 
@@ -24,7 +25,6 @@ class Vertex {
  * Graph class
  */
 class Graph {
-
   /**
    * Constructor
    */
@@ -45,8 +45,13 @@ class Graph {
    */
   findVertex(value) {
     // !!! IMPLEMENT ME
+    for (let i = 0; i < this.vertexes.length; i++) {
+      if (this.vertexes[i].value === value) {
+        return this.vertexes[i];
+      }
+    }
+    return null;
   }
-
   /**
    * Breadth-First search from a starting vertex. This should keep parent
    * pointers back from neighbors to their parent.
@@ -55,8 +60,34 @@ class Graph {
    */
   bfs(start) {
     // !!! IMPLEMENT ME
-  }
+    let queue = [];
+    let visited = [];
 
+    for (let v of graph.vertexes) {
+      v.color = 'white';
+      v.parent = null;
+    }
+
+    start.color = 'gray';
+    queue.push(start);
+
+    while (queue.length > 0) {
+      let u = queue[0];
+
+      for (let v of u.edges) {
+        if (v.destination.color === 'white') {
+          v.destination.color = 'gray';
+          v.destination.parent = u;
+          queue.push(v.destination);
+        }
+      }
+
+      let parent = queue.shift();
+      visited.push(parent);
+      u.color = 'black';
+    }
+    return visited;
+  }
   /**
    * Print out the route from the start vert back along the parent
    * pointers (set in the previous BFS)
@@ -66,6 +97,15 @@ class Graph {
    */
   outputRoute(start) {
     // !!! IMPLEMENT ME
+
+    let result = [start.value]; // make result array with start value
+    let current = start; // set current to start value
+    while (current.parent !== null) {
+      // while current has a parent
+      result.push(current.parent.value); // add current's parent value to result array
+      current = current.parent; // make current's parent the new current
+    }
+    console.log(result.join(' --> ')); // print the array as a string with arrow pointers
   }
 
   /**
